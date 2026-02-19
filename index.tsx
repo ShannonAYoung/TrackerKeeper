96a8fa0d9d28f16161b4151d4d08038ac8c06b1a
Shannontwin <Young.Shannon.A@gmail.com>
2026-02-19 18:55:42 -0500

    Reverted file.

--- index.tsx
+++ index.tsx
@@ -1,11 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { JSDOM } from 'jsdom';
-
-const dom = new JSDOM(`<!DOCTYPE html><div id="root"></div>`);
-const document = dom.window.document;
-
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
