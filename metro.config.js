22e263443706bc6ddb25d3406896b94bc8d07ad0
Shannontwin <Young.Shannon.A@gmail.com>
2026-03-06 21:01:18 -0500

    Created new file metro.config.js and updated package.json.

---
+++ metro.config.js
@@ -0,0 +1,23 @@
+/ metro.config.js
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+
+const defaultConfig = getDefaultConfig(__dirname);
+
+/** @type {import('metro-config').MetroConfig} */
+const customConfig = {
+  resolver: {
+    // Ensure TypeScript extensions are included
+    sourceExts: [
+      ...defaultConfig.resolver.sourceExts,
+      'ts',
+      'tsx'
+    ]
+  },
+  transformer: {
+    // If you later add SVG support, uncomment this:
+    // babelTransformerPath: require.resolve('react-native-svg-transformer'),
+  }
+};
+
+module.exports = mergeConfig(defaultConfig, customConfig);
+
