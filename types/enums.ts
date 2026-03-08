2f29a6ce54cbf714d5178fbbb3fdde580a830441
Shannontwin <Young.Shannon.A@gmail.com>
2026-03-08 11:59:44 -0400

    Created enums.ts.

---
+++ types/enums.ts
@@ -0,0 +1,11 @@
+export enum Platform {
+  UNKNOWN = "UNKNOWN",
+  IOS = "iOS",
+  ANDROID = "Android"
+}
+
+export enum ConnectionStatus {
+  DISCONNECTED = "DISCONNECTED",
+  SEARCHING = "SEARCHING",
+  CONNECTED = "CONNECTED"
+}
