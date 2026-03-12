43ab1756bd332f7354e8ade606abaa7aa474c31e
Shannontwin <Young.Shannon.A@gmail.com>
2026-03-11 20:34:17 -0400

    Updated vite.config.ts.

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from "@vitejs/plugin-react/dist/index.js";
+
+
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
